efebea33398d3edee29535778b8e5581d109b53d
Cem <cemakmaz@hotmail.com>
2021-02-02 02:21:26 +0300

    Added while loop

--- main.py
+++ main.py
@@ -2,24 +2,47 @@ import praw
 import os
 import config
 import random
+from datetime import datetime
+import time
 
+#Config
 reddit = praw.Reddit(client_id = config.clientID,
                     client_secret = config.clientSecret,
                     username = config.redditID,
                     password = config.redditPassword,
                     user_agent = config.userAgent,)
 
-subName = random.choice(["disneyvacation", "notdisneyvacation"])
-print(subName)
+#Posts the submisson
+def postSubmisson():
 
-for submission in reddit.subreddit(subName).new(limit=20):
-    if not submission.stickied and not submission.is_self and not submission.saved and submission.score > 5:
-        print(submission.title)
-        print(submission.url)
-        submission.save()
-        posted = reddit.subreddit("disneydilemma").submit(submission.title, url=submission.url)
-        print(posted.permalink)
-        posted.reply("Crossposted from r/" + ">!" + subName + " " + submission.shortlink + "!<")
-        break
+    #Picks DisneyVacation or NotDisneyVacation sub
+    subName = random.choice(["DisneyVacation", "NotDisneyVacation"])
+    print(subName)
+
+    #Searches the new for a suitable post
+    for submission in reddit.subreddit(subName).new(limit=50):
+        if not submission.stickied and not submission.is_self and not submission.saved and submission.score > 5:
+            print(submission.title)
+            print(submission.url)
+            submission.save()
+            #Adds nsfw tag if needed
+            if submission.over_18 :
+                posted = reddit.subreddit("disneydilemma").submit(submission.title, url=submission.url, nsfw=True)
+            else :
+                posted = reddit.subreddit("disneydilemma").submit(submission.title, url=submission.url)
+            print(posted.permalink)
+            #Comments the real subreddit and the link
+            posted.reply("Crossposted from r/" + ">!" + subName + " " + submission.shortlink + "!<")
+            break
+        else:
+            print("No Suitable Post Left")
+
+#Runs the code every 15 minutes
+while True:
+    print("Update data:", datetime.now())
+    sleep = 15 - datetime.now().minute % 15
+    if sleep == 15:
+        postSubmisson()
+        time.sleep(sleep * 60)
     else:
-        print("No Suitable Post Left")
+        time.sleep(sleep * 60)
